047d4e37cdbf8b38fb6dab76f557822220cffc2f
EAChowfan <mioleeldridgeaaron5@gmail.com>
2026-02-01 21:47:25 +0800

    Added logo and set up tailwind

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { Header } from './Header.tsx'
+import './css/App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    <div className="h-12">
+    <main className="pt-16 p-4 centered">
+      <h1 className="text-3xl font-bold">Your Financial Application</h1>
+    </main>
+    </div>
+    </>
+  )
+}
+
+export default App
